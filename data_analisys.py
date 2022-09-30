7c407ee6a5079b61eb5752ea020a9435272e62f3
raphael <raphael.gomes@enacom.com.br>
2022-09-30 09:14:22 -0300

    Decreasing cognitive complexity of the code

--- data_analisys.py
+++ data_analisys.py
@@ -6,33 +6,50 @@ import os
 from copy import deepcopy
 
 
-def data_analysis(df_customers: pandas.DataFrame, df_suppliers: pandas.DataFrame):
-    """
-    This method calculates the mais statistics for a greenfield analysis. A plot that
-    shows a position analysis is saved to be printed in the web page.
+def data_analysis(df_customers_db: pandas.DataFrame, df_suppliers_db: pandas.DataFrame) -> (dict, pd.DataFrame):
 
-    Args:
-        df_customers (pandas.DataFrame): dataframe with customers information
-        df_suppliers (pandas.DataFrame): dataframe with suppliers information
+    df_customers = deepcopy(df_customers_db)
+    df_suppliers = deepcopy(df_suppliers_db)
 
-    Returns:
-         (dict): main statistics
-         (pandas.DataFrame): information about suppliers and customers
-    """
+    index_max_demand = df_customers['Fixed Demand'].idxmax()
+    index_min_demand = df_customers['Fixed Demand'].idxmin()
+    index_max_supply = df_suppliers['Maximum Supply'].idxmax()
+    index_min_supply = df_suppliers['Maximum Supply'].idxmin()
 
-    customers = deepcopy(df_customers)
-    suppliers = deepcopy(df_suppliers)
-    df_distances = pd.DataFrame(np.ones([len(customers), len(suppliers)]), columns=suppliers['Name'].values,
-                                index=customers['Name'].values)
+    mean_values, df_distances = calculate_distance(df_customers, df_suppliers)
+
+    statistics = {'maximum_demand': {'Name': df_customers['Name'][index_max_demand],
+                                     'Value': df_customers['Fixed Demand'].max()},
+                  'minimum_demand': {'Name': df_customers['Name'][index_min_demand],
+                                     'Value': df_customers['Fixed Demand'].min()},
+                  'maximum_supply': {'Name': df_suppliers['Name'][index_max_supply],
+                                     'Value': df_suppliers['Maximum Supply'].max()},
+                  'minimum_supply': {'Name': df_suppliers['Name'][index_min_supply],
+                                     'Value': df_suppliers['Maximum Supply'].min()},
+                  'sum_demand': df_customers['Fixed Demand'].sum(),
+                  'sum_supply': df_suppliers['Maximum Supply'].sum(),
+                  }
+
+    statistics.update(mean_values)
+
+    plot_data_analysis(df_customers, df_suppliers)
+
+    return statistics, df_distances
+
+
+def calculate_distance(df_customers: pandas.DataFrame, df_suppliers: pandas.DataFrame) -> (dict, pd.DataFrame):
 
     mean_values = {}
-    for supplier in suppliers.transpose():
-        for customer in customers.transpose():
+    df_distances = pd.DataFrame(np.ones([len(df_customers), len(df_suppliers)]), columns=df_suppliers['Name'].values,
+                                index=df_customers['Name'].values)
+
+    for supplier in df_suppliers.transpose():
+        for customer in df_customers.transpose():
             pi_rad = np.pi / 180
-            x1 = float(customers['Longitude'][customer]) * pi_rad
-            y1 = float(customers['Latitude'][customer]) * pi_rad
-            x2 = float(suppliers['Longitude'][supplier]) * pi_rad
-            y2 = float(suppliers['Latitude'][supplier]) * pi_rad
+            x1 = float(df_customers['Longitude'][customer]) * pi_rad
+            y1 = float(df_customers['Latitude'][customer]) * pi_rad
+            x2 = float(df_suppliers['Longitude'][supplier]) * pi_rad
+            y2 = float(df_suppliers['Latitude'][supplier]) * pi_rad
 
             delta_x = abs(x1 - x2)
             delta_y = abs(y1 - y2)
@@ -41,41 +58,28 @@ def data_analysis(df_customers: pandas.DataFrame, df_suppliers: pandas.DataFrame
 
             c = 2 * np.arctan(np.sqrt(a) / np.sqrt(1 - a))
 
-            distance = 6371 * c
-            df_distances[suppliers['Name'][supplier]][customers['Name'][customer]] = distance
+            earth_ray = 6371
+            distance = earth_ray * c
+            df_distances[df_suppliers['Name'][supplier]][df_customers['Name'][customer]] = distance
 
-        mean_values[suppliers['Name'][supplier]] = df_distances[suppliers['Name'][supplier]].mean()
+        mean_values[df_suppliers['Name'][supplier]] = df_distances[df_suppliers['Name'][supplier]].mean()
 
-    index_max_demand = customers['Fixed Demand'].idxmax()
-    index_min_demand = customers['Fixed Demand'].idxmin()
-    index_max_supply = suppliers['Maximum Supply'].idxmax()
-    index_min_supply = suppliers['Maximum Supply'].idxmin()
+    return mean_values, df_distances
 
-    statistics = {'maximum_demand': {'Name': customers['Name'][index_max_demand],
-                                     'Value': customers['Fixed Demand'].max()},
-                  'minimum_demand': {'Name': customers['Name'][index_min_demand],
-                                     'Value': customers['Fixed Demand'].min()},
-                  'maximum_supply': {'Name': suppliers['Name'][index_max_supply],
-                                     'Value': suppliers['Maximum Supply'].max()},
-                  'minimum_supply': {'Name': suppliers['Name'][index_min_supply],
-                                     'Value': suppliers['Maximum Supply'].min()},
-                  'sum_demand': customers['Fixed Demand'].sum(),
-                  'sum_supply': suppliers['Maximum Supply'].sum(),
-                  }
 
-    statistics.update(mean_values)
+def plot_data_analysis(df_customers: pd.DataFrame, df_suppliers: pd.DataFrame):
 
-    customers['ID'] = ['Customer'] * len(customers)
-    suppliers['ID'] = ['Supplier'] * len(suppliers)
-    customers['Size'] = [demand * 30 / customers['Fixed Demand'].max()
-                         for demand in customers['Fixed Demand'].values.tolist()]
-    suppliers['Size'] = [supply * 30 / suppliers['Maximum Supply'].max()
-                         for supply in suppliers['Maximum Supply'].values.tolist()]
+    df_customers['ID'] = ['Customer'] * len(df_customers)
+    df_suppliers['ID'] = ['Supplier'] * len(df_suppliers)
+    df_customers['Size'] = [demand * 30 / df_customers['Fixed Demand'].max()
+                            for demand in df_customers['Fixed Demand'].values.tolist()]
+    df_suppliers['Size'] = [supply * 30 / df_suppliers['Maximum Supply'].max()
+                            for supply in df_suppliers['Maximum Supply'].values.tolist()]
 
-    customers.drop(['Fixed Demand'], axis=1, inplace=True)
-    suppliers.drop(['Maximum Supply'], axis=1, inplace=True)
+    df_customers.drop(['Fixed Demand'], axis=1, inplace=True)
+    df_suppliers.drop(['Maximum Supply'], axis=1, inplace=True)
 
-    df_all = pd.concat([customers, suppliers])
+    df_all = pd.concat([df_customers, df_suppliers])
 
     fig = px.scatter(df_all, x="Longitude", y="Latitude", color="ID", size='Size', text='Name')
 
@@ -83,5 +87,3 @@ def data_analysis(df_customers: pandas.DataFrame, df_suppliers: pandas.DataFrame
         os.remove('static/fig.jpeg')
 
     fig.write_image('static/fig.jpeg')
-
-    return statistics, df_all, df_distances
--- server.py
+++ server.py
@@ -2,9 +2,8 @@ import pandas as pd
 from flask import Flask, render_template, request
 import os
 from error_analysis import ErrorAnalysis
-import sys
 from html_generator import html_generator
-from insert_data import inserting_data, exporting_data
+from database_management import inserting_data, exporting_data
 from copy import deepcopy
 from data_analisys import data_analysis
 
@@ -47,49 +46,66 @@ def error_report():
         alerts, errors = error_analysis.errors_search()
 
         if not errors:
-            df_customers = deepcopy(error_analysis.customers)
-            df_suppliers = deepcopy(error_analysis.suppliers)
-            df_locations = deepcopy(error_analysis.locations)
-            customers_names = df_customers['Name'].values.tolist()
-            suppliers_names = df_suppliers['Name'].values.tolist()
+            df_customers, df_suppliers = adjusting_data(error_analysis.customers, error_analysis.suppliers,
+                                                        error_analysis.locations)
 
-            df_customers['Latitude'] = [df_locations['Latitude'][name] for name in customers_names]
-            df_customers['Longitude'] = [df_locations['Longitude'][name] for name in customers_names]
-            df_suppliers['Latitude'] = [df_locations['Latitude'][name] for name in suppliers_names]
-            df_suppliers['Longitude'] = [df_locations['Longitude'][name] for name in suppliers_names]
-
-            customers_db, suppliers_db = exporting_data()
-
-            customers_info = [(customers_db['Name'][i], customers_db['Latitude'][i], customers_db['Longitude'][i])
-                              for i in range(len(customers_db))]
-
-            suppliers_info = [(suppliers_db['Name'][i], suppliers_db['Latitude'][i], suppliers_db['Longitude'][i])
-                              for i in range(len(suppliers_db))]
-
-            for name in df_customers.transpose():
-                customers_tuple = (df_customers['Name'][name], df_customers['Latitude'][name],
-                                   df_customers['Longitude'][name])
-                if customers_tuple in customers_info and df_customers['Status'][name] == 'Include':
-                    df_customers.drop([name], axis=0, inplace=True)
-
-            for name in df_suppliers.transpose():
-                suppliers_tuple = (df_suppliers['Name'][name], df_suppliers['Latitude'][name],
-                                   df_suppliers['Longitude'][name])
-                if suppliers_tuple in suppliers_info and df_suppliers['Status'][name] == 'Include':
-                    df_suppliers.drop([name], axis=0, inplace=True)
+            df_customers, df_suppliers = checking_database_for_duplicates(df_customers, df_suppliers)
 
             inserting_data(df_customers, df_suppliers)
 
             df_customers_db, df_suppliers_db = exporting_data()
 
             if not df_suppliers_db.empty and not df_customers_db.empty:
-                statistics, df_all, df_distances = data_analysis(df_customers_db, df_suppliers_db)
+                statistics, df_distances = data_analysis(df_customers_db, df_suppliers_db)
 
         html_generator(alerts, errors, df_customers_db, df_suppliers_db, statistics, df_distances)
 
         os.remove(path)
 
         return render_template('html_report.html')
+
+
+def adjusting_data(customers: pd.DataFrame, suppliers: pd.DataFrame,
+                   locations: pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
+
+    df_customers = deepcopy(customers)
+    df_suppliers = deepcopy(suppliers)
+    df_locations = deepcopy(locations)
+    customers_names = df_customers['Name'].values.tolist()
+    suppliers_names = df_suppliers['Name'].values.tolist()
+
+    df_customers['Latitude'] = [df_locations['Latitude'][name] for name in customers_names]
+    df_customers['Longitude'] = [df_locations['Longitude'][name] for name in customers_names]
+    df_suppliers['Latitude'] = [df_locations['Latitude'][name] for name in suppliers_names]
+    df_suppliers['Longitude'] = [df_locations['Longitude'][name] for name in suppliers_names]
+
+    return df_customers, df_suppliers
+
+
+def checking_database_for_duplicates(df_customers: pd.DataFrame,
+                                     df_suppliers: pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
+
+    customers_db, suppliers_db = exporting_data()
+
+    customers_info = [(customers_db['Name'][i], customers_db['Latitude'][i], customers_db['Longitude'][i])
+                      for i in range(len(customers_db))]
+
+    suppliers_info = [(suppliers_db['Name'][i], suppliers_db['Latitude'][i], suppliers_db['Longitude'][i])
+                      for i in range(len(suppliers_db))]
+
+    for name in df_customers.transpose():
+        customers_tuple = (df_customers['Name'][name], df_customers['Latitude'][name],
+                           df_customers['Longitude'][name])
+        if customers_tuple in customers_info and df_customers['Status'][name] == 'Include':
+            df_customers.drop([name], axis=0, inplace=True)
+
+    for name in df_suppliers.transpose():
+        suppliers_tuple = (df_suppliers['Name'][name], df_suppliers['Latitude'][name],
+                           df_suppliers['Longitude'][name])
+        if suppliers_tuple in suppliers_info and df_suppliers['Status'][name] == 'Include':
+            df_suppliers.drop([name], axis=0, inplace=True)
+
+    return df_customers, df_suppliers
 
 
 if __name__ == '__main__':
